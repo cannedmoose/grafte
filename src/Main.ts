8c508459722ba24e88b876d5ebc11aa76df78983
Callan McNamara <callan.mcnamara@gmail.com>
2020-01-14 11:12:51 +1100

    simple circle tool

--- src/Main.ts
+++ src/Main.ts
@@ -13,12 +13,34 @@ function rtoPoint(rect: paper.Rectangle) {
 
 window.onload = function() {
   let canvas: HTMLCanvasElement = document.querySelector("#canvas");
-  let project = new paper.Project(canvas);
-  project.activate();
+  paper.setup(canvas);
   let path = new paper.Path();
 
-  path.add([0, 0]);
-  path.add(rtoPoint(paper.view.bounds));
+  path.moveTo(new paper.Point(0, 0));
+  path.lineTo(rtoPoint(paper.view.bounds));
   path.strokeColor = new paper.Color("black");
   path.strokeWidth = 10;
+
+  paper.view.onResize = event => {
+    path.lastSegment.point = rtoPoint(paper.view.bounds);
+  };
+
+  let circleTool = new paper.Tool();
+  let circlePath = new paper.Path();
+
+  circleTool.onMouseDrag = function(event) {
+    circlePath.removeSegments();
+    circlePath = new paper.Path.Circle({
+      center: event.downPoint,
+      radius: event.downPoint.getDistance(event.point)
+    });
+    circlePath.strokeColor = new paper.Color("blue");
+    circlePath.fillColor = new paper.Color(0, 0, 0, 0);
+  };
+
+  circleTool.onMouseUp = function(event) {
+    circlePath.strokeColor = new paper.Color("black");
+    circlePath.fillColor = new paper.Color("white");
+    circlePath = new paper.Path();
+  };
 };

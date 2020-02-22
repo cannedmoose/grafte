f471f500d32924570669715d6ee5ac2d08922bfb
Callan McNamara <callan.mcnamara@gmail.com>
2020-02-23 08:54:29 +1100

    Change path slighty,

--- src/tools/pen.ts
+++ src/tools/pen.ts
@@ -6,30 +6,33 @@ export function penTool({ canvas }): paper.Tool {
 
   penTool.minDistance = 1;
   let path: paper.Path | undefined;
+  let segment: paper.Segment | undefined;
 
   penTool.onMouseDown = function(event: paper.ToolEvent) {
     if (path) {
       if (path.lastSegment.point.isClose(event.point, 6)) {
-        path.selected = false;
         path = undefined;
       } else if (path.firstSegment.point.isClose(event.point, 6)) {
         path.closePath();
-        path.selected = false;
+        segment = path.firstSegment;
         path = undefined;
       } else {
         path.add(event.point);
+        segment = path.lastSegment;
       }
     } else {
+      paper.project.deselectAll();
       path = new paper.Path([event.downPoint]);
-      path.fullySelected = true;
+      segment = path.firstSegment;
+      path.selected = true;
     }
   };
 
   penTool.onMouseDrag = function(event: paper.ToolEvent) {
-    if (!path) return;
-    path.fullySelected = true;
-    path.lastSegment.handleOut = event.downPoint.subtract(event.point);
-    path.lastSegment.handleIn = path.lastSegment.handleOut.multiply(-1);
+    if(!segment) return;
+    segment.selected = true;
+    segment.handleIn = event.downPoint.subtract(event.point);
+    segment.handleOut = segment.handleIn.multiply(-1);
   };
 
   penTool.onMouseUp = function(event: paper.ToolEvent) {
